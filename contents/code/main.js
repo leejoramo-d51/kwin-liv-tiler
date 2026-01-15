4358ca412514337d12e1bbdda78c897bae90a3bb
Lee Joramo <lee.joramo@d51schools.org>
2026-01-15 10:46:42 -0700

    ADD window margin configurable via GUI

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,75 +1,110 @@
+var margin = readConfig("WindowMargin", 10);
+options.configChanged.connect(function () {
+  var newWidth = readConfig("MyIntegerInput", 10);
+  print("Config changed! New width: " + newWidth);
+});
 var tilings = {};
 
-tileWindowToTheLeftOfScreen = (window, screen) => {
+tileWindowToTheLeftOfScreen = (window, screen, area, quadWidth, quadHeight) => {
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height
+    x: area.x + margin,
+    y: area.y + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight - 2 * margin,
   };
 
   tilings[window] = tileWindowToTheLeftOfScreen;
-}
+};
 
-tileWindowToTheTopLeftOfScreen = (window, screen) => {
+tileWindowToTheTopLeftOfScreen = (
+  window,
+  screen,
+  area,
+  quadWidth,
+  quadHeight,
+) => {
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height / 2
+    x: area.x + margin,
+    y: area.y + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = tileWindowToTheTopLeftOfScreen;
-}
+};
 
-tileWindowToTheBottomLeftOfScreen = (window, screen) => {
+tileWindowToTheBottomLeftOfScreen = (
+  window,
+  screen,
+  area,
+  quadWidth,
+  quadHeight,
+) => {
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y + screen.geometry.height / 2,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height / 2
+    x: area.x + margin,
+    y: area.y + quadHeight / 2 + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = tileWindowToTheBottomLeftOfScreen;
-}
+};
 
-tileWindowToTheRightOfScreen = (window, screen) => {
+tileWindowToTheRightOfScreen = (
+  window,
+  screen,
+  area,
+  quadWidth,
+  quadHeight,
+) => {
   window.frameGeometry = {
-    x: screen.geometry.x + screen.geometry.width / 2,
-    y: screen.geometry.y,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height
+    x: area.x + quadWidth / 2 + margin,
+    y: area.y + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight - 2 * margin,
   };
 
   tilings[window] = tileWindowToTheRightOfScreen;
-}
+};
 
-tileWindownToTheTopRightOfScreen = (window, screen) => {
+tileWindownToTheTopRightOfScreen = (
+  window,
+  screen,
+  area,
+  quadWidth,
+  quadHeight,
+) => {
   window.frameGeometry = {
-    x: screen.geometry.x + screen.geometry.width / 2,
-    y: screen.geometry.y,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height / 2
+    x: area.x + quadWidth / 2 + margin,
+    y: area.y + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = tileWindownToTheTopRightOfScreen;
-}
+};
 
-tileWindowToTheBottomRightOfScreen = (window, screen) => {
+tileWindowToTheBottomRightOfScreen = (
+  window,
+  screen,
+  area,
+  quadWidth,
+  quadHeight,
+) => {
   window.frameGeometry = {
-    x: screen.geometry.x + screen.geometry.width / 2,
-    y: screen.geometry.y + screen.geometry.height / 2,
-    width: screen.geometry.width / 2,
-    height: screen.geometry.height / 2
+    x: area.x + quadWidth / 2 + margin,
+    y: area.y + quadHeight / 2 + margin,
+    width: quadWidth / 2 - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = tileWindowToTheBottomRightOfScreen;
-}
+};
 
 wrappingToTheLeft = (leftTiler, rightTiler) => {
-  return (window, screen) => {
+  return (window, screen, area, quadWidth, quadHeight) => {
     if (!tilings[window] || tilings[window] !== leftTiler) {
-      leftTiler(window, screen);
+      leftTiler(window, screen, area, quadWidth, quadHeight);
     } else if (screen.geometry.x > 0) {
       let chosen = null;
 
@@ -80,7 +115,11 @@ wrappingToTheLeft = (leftTiler, rightTiler) => {
         }
 
         if (candidate.geometry.x > screen.geometry.x) {
-          print("Skipping screen", candidate.geometry, "because it is further to the right than the current screen");
+          print(
+            "Skipping screen",
+            candidate.geometry,
+            "because it is further to the right than the current screen",
+          );
           continue;
         }
 
@@ -92,25 +131,47 @@ wrappingToTheLeft = (leftTiler, rightTiler) => {
           continue;
         } else if (candidate.geometry.x > chosen.geometry.x) {
           chosen = candidate;
-          print("Chose", chosen.geometry, "because", candidate.geometry, "is further to the right");
+          print(
+            "Chose",
+            chosen.geometry,
+            "because",
+            candidate.geometry,
+            "is further to the right",
+          );
         }
       }
 
       if (chosen !== null) {
         print("Moving to screen", chosen.geometry);
-        rightTiler(window, chosen);
+        let chosenArea = workspace.clientArea(
+          KWin.MaximizeArea,
+          chosen,
+          workspace.currentDesktop,
+        );
+        let chosenQuadWidth = Math.floor(chosenArea.width);
+        let chosenQuadHeight = Math.floor(chosenArea.height);
+        rightTiler(
+          window,
+          chosen,
+          chosenArea,
+          chosenQuadWidth,
+          chosenQuadHeight,
+        );
       }
     }
-  }
-}
+  };
+};
 
 wrappingToTheRight = (leftTiler, rightTiler) => {
-  return (window, screen) => {
+  return (window, screen, area, quadWidth, quadHeight) => {
     let virtualScreenGeometry = workspace.virtualScreenGeometry;
 
     if (!tilings[window] || tilings[window] !== rightTiler) {
-      rightTiler(window, screen);
-    } else if (screen.geometry.x + screen.geometry.width < virtualScreenGeometry.x + virtualScreenGeometry.width) {
+      rightTiler(window, screen, area, quadWidth, quadHeight);
+    } else if (
+      screen.geometry.x + screen.geometry.width <
+      virtualScreenGeometry.x + virtualScreenGeometry.width
+    ) {
       let chosen = null;
 
       for (const candidate of workspace.screens) {
@@ -119,8 +180,15 @@ wrappingToTheRight = (leftTiler, rightTiler) => {
           continue;
         }
 
-        if (candidate.geometry.x + candidate.geometry.width < screen.geometry.x + screen.geometry.width) {
-          print("Skipping screen", candidate.geometry, "because it is further to the left than the current screen");
+        if (
+          candidate.geometry.x + candidate.geometry.width <
+          screen.geometry.x + screen.geometry.width
+        ) {
+          print(
+            "Skipping screen",
+            candidate.geometry,
+            "because it is further to the left than the current screen",
+          );
           continue;
         }
 
@@ -130,54 +198,76 @@ wrappingToTheRight = (leftTiler, rightTiler) => {
           chosen = candidate;
           print("Chose", chosen.geometry, "because chosen === null");
           continue;
-        } else if (candidate.geometry.x + candidate.geometry.width < chosen.geometry.x + chosen.geometry.width) {
+        } else if (
+          candidate.geometry.x + candidate.geometry.width <
+          chosen.geometry.x + chosen.geometry.width
+        ) {
           chosen = candidate;
-          print("Chose", chosen.geometry, "because", candidate.geometry, "is further to the left");
+          print(
+            "Chose",
+            chosen.geometry,
+            "because",
+            candidate.geometry,
+            "is further to the left",
+          );
         }
       }
 
       if (chosen !== null) {
         print("Moving to screen", chosen.geometry);
-        leftTiler(window, chosen);
+        let chosenArea = workspace.clientArea(
+          KWin.MaximizeArea,
+          chosen,
+          workspace.currentDesktop,
+        );
+        let chosenQuadWidth = Math.floor(chosenArea.width);
+        let chosenQuadHeight = Math.floor(chosenArea.height);
+        leftTiler(
+          window,
+          chosen,
+          chosenArea,
+          chosenQuadWidth,
+          chosenQuadHeight,
+        );
       }
     }
-  }
-}
+  };
+};
 
-theTop = (window, screen) => {
+theTop = (window, screen, area, quadWidth, quadHeight) => {
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y,
-    width: screen.geometry.width,
-    height: screen.geometry.height / 2
+    x: area.x + margin,
+    y: area.y + margin,
+    width: quadWidth - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = theTop;
-}
+};
 
-theBottom = (window, screen) => {
+theBottom = (window, screen, area, quadWidth, quadHeight) => {
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y + screen.geometry.height / 2,
-    width: screen.geometry.width,
-    height: screen.geometry.height / 2
+    x: area.x + margin,
+    y: area.y + quadHeight / 2 + margin,
+    width: quadWidth - 2 * margin,
+    height: quadHeight / 2 - 2 * margin,
   };
 
   tilings[window] = theBottom;
-}
+};
 
-var wholeScreened = {}
+var wholeScreened = {};
 
-theWholeScreen = (window, screen) => {
+theWholeScreen = (window, screen, area, quadWidth, quadHeight) => {
   if (tilings[window] == theWholeScreen && wholeScreened[window]) {
-    return wholeScreened[window](window, screen);
+    return wholeScreened[window](window, screen, area, quadWidth, quadHeight);
   }
 
   window.frameGeometry = {
-    x: screen.geometry.x,
-    y: screen.geometry.y,
-    width: screen.geometry.width,
-    height: screen.geometry.height
+    x: area.x + margin,
+    y: area.y + margin,
+    width: quadWidth - 2 * margin,
+    height: quadHeight - 2 * margin,
   };
 
   if (tilings[window] && tilings[window] != theWholeScreen) {
@@ -185,7 +275,7 @@ theWholeScreen = (window, screen) => {
   }
 
   tilings[window] = theWholeScreen;
-}
+};
 
 setUp = (window) => {
   window.setMaximize(false, false);
@@ -195,9 +285,9 @@ setUp = (window) => {
     window.closed.connect(() => {
       print("Window", window, "closed, deleting tiling data");
       delete tilings[window];
-    })
+    });
   }
-}
+};
 
 tileWindowTo = (pos, tiler) => {
   return () => {
@@ -205,26 +295,52 @@ tileWindowTo = (pos, tiler) => {
 
     let window = workspace.activeWindow;
     let screen = window.output;
-
+    let area = workspace.clientArea(KWin.MaximizeArea, window);
+    let quadWidth = area.width;
+    let quadHeight = area.height;
     if (window.desktopWindow) {
       print("Window is a desktop window, ignoring");
       return;
     }
 
     setUp(window);
-    tiler(window, screen);
+    tiler(window, screen, area, quadWidth, quadHeight);
   };
-}
+};
 
-register = (direction, shortcut, tiler) => registerShortcut("Really Tile Window to the " + direction, "", shortcut, tileWindowTo(direction, tiler));
+register = (direction, shortcut, tiler) =>
+  registerShortcut(
+    "Really Tile Window to the " + direction,
+    "",
+    shortcut,
+    tileWindowTo(direction, tiler),
+  );
 
-theLeft = wrappingToTheLeft(tileWindowToTheLeftOfScreen, tileWindowToTheRightOfScreen);
-theTopLeft = wrappingToTheLeft(tileWindowToTheTopLeftOfScreen, tileWindownToTheTopRightOfScreen);
-theBottomLeft = wrappingToTheLeft(tileWindowToTheBottomLeftOfScreen, tileWindowToTheBottomRightOfScreen);
+theLeft = wrappingToTheLeft(
+  tileWindowToTheLeftOfScreen,
+  tileWindowToTheRightOfScreen,
+);
+theTopLeft = wrappingToTheLeft(
+  tileWindowToTheTopLeftOfScreen,
+  tileWindownToTheTopRightOfScreen,
+);
+theBottomLeft = wrappingToTheLeft(
+  tileWindowToTheBottomLeftOfScreen,
+  tileWindowToTheBottomRightOfScreen,
+);
 
-theRight = wrappingToTheRight(tileWindowToTheLeftOfScreen, tileWindowToTheRightOfScreen);
-theTopRight = wrappingToTheRight(tileWindowToTheTopLeftOfScreen, tileWindownToTheTopRightOfScreen);
-theBottomRight = wrappingToTheRight(tileWindowToTheBottomLeftOfScreen, tileWindowToTheBottomRightOfScreen);
+theRight = wrappingToTheRight(
+  tileWindowToTheLeftOfScreen,
+  tileWindowToTheRightOfScreen,
+);
+theTopRight = wrappingToTheRight(
+  tileWindowToTheTopLeftOfScreen,
+  tileWindownToTheTopRightOfScreen,
+);
+theBottomRight = wrappingToTheRight(
+  tileWindowToTheBottomLeftOfScreen,
+  tileWindowToTheBottomRightOfScreen,
+);
 
 register("Whole Screen", "Meta+Shift+i", theWholeScreen);
 
